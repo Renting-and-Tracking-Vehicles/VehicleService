7ece8b067ceac5c0e8c7ba74fc602f5d4a824fac
Marija Klještan <73355880+marijakljestan@users.noreply.github.com>
2022-03-25 09:19:40 +0100

    feat: Initial model and database configuration (#2)

--- backend/src/main/java/com/example/vehiclesservice/model/Address.java
+++ backend/src/main/java/com/example/vehiclesservice/model/Address.java
@@ -3,16 +3,29 @@ package com.example.vehiclesservice.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import javax.persistence.*;
 
 @Getter@Setter@NoArgsConstructor
+@Entity
 public class Address {
 
+    @Id
+    @SequenceGenerator(name = "addressSeqGen", sequenceName = "addressSeqGen", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "addressSeqGen")
+    @Column(name="address_id", unique=true, nullable=false)
     private int id;
-    private String street;
-    private String number;
+    @Column(name="street_name", nullable=false)
+    private String streetName;
+    @Column(name="street_number", nullable=false)
+    private String streetNumber;
+    @Column(name="city", nullable=false)
     private String city;
-    private String postalcode;
+    @Column(name="postal_code")
+    private String postalCode;
+    @Column(name="country", nullable = false)
     private String country;
+    @Column(name="longitude", nullable = false)
     private double longitude;
+    @Column(name="latitude", nullable = false)
     private double latitude;
 }
--- backend/src/main/java/com/example/vehiclesservice/model/Vehicle.java
+++ backend/src/main/java/com/example/vehiclesservice/model/Vehicle.java
@@ -3,16 +3,29 @@ package com.example.vehiclesservice.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 @Getter@Setter@NoArgsConstructor
 public class Vehicle {
+    @Id
+    @SequenceGenerator(name = "vehicleSeqGen", sequenceName = "vehicleSeqGen", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "vehicleSeqGen")
+    @Column(name="vehicle_id", unique=true, nullable=false)
     private int id;
+    @Column(name="model", nullable=false)
     private String model;
+    @Column(name="description", nullable=false)
     private String description;
+    @Column(name="price_per_day", nullable=false)
     private double pricePerDay;
-    private Address address;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "garage_id")
+    private Garage garage;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "vehicle_images", joinColumns = @JoinColumn(name = "vehicle_id"))
     private Set<String> images;
+    @Column(name="is_rented", nullable=false)
     private Boolean rented;
 }
