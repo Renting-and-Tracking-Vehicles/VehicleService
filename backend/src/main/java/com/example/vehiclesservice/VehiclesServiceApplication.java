bc41efa7e0e0914a9fae9f1fdbbdbb3427c59771
Marija Klještan <marijakljestan@uns.ac.rs>
2022-04-08 14:11:05 +0200

    feat: Aws configuration

--- backend/src/main/java/com/example/vehiclesservice/VehiclesServiceApplication.java
+++ backend/src/main/java/com/example/vehiclesservice/VehiclesServiceApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ContextStackAutoConfiguration.class, ContextRegionProviderAutoConfiguration.class})
 @EnableEurekaClient
 @EnableFeignClients(clients = UserServiceApi.class)
 public class VehiclesServiceApplication {
