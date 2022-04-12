f9c1137dfae50fbe79f0fb155eecec26f590d718
Marija Klještan <marijakljestan@uns.ac.rs>
2022-04-12 13:55:07 +0200

    refactror: Optimistic lock handling

--- backend/src/main/java/com/example/vehiclesservice/service/RentingServiceImpl.java
+++ backend/src/main/java/com/example/vehiclesservice/service/RentingServiceImpl.java
@@ -8,8 +8,11 @@ import com.example.vehiclesservice.model.RentingEntity;
 import com.example.vehiclesservice.repository.RentingRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.OptimisticLockException;
 import java.util.List;
@@ -27,20 +30,24 @@ public class RentingServiceImpl implements RentingService{
     @Override
     @Transactional(readOnly = false)
     public Renting startRenting(Renting renting) {
-        RentingEntity rentingEntity = modelMapper.map(renting, RentingEntity.class);
-        changeCarStatusToRented(renting.getVehicle());
-        rentingRepository.save(rentingEntity);
+        try {
+            RentingEntity rentingEntity = modelMapper.map(renting, RentingEntity.class);
+            changeCarStatusToRented(renting.getVehicle());
+        }catch(ObjectOptimisticLockingFailureException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This vehicle is already reserved!");
+        }
         return renting;
     }
 
     @Override
-    public Renting finishRenting(Renting renting) throws  OptimisticLockException{
+    public Renting finishRenting(Renting renting) {
         changeCarStatusToAvailable(renting.getVehicle());
         RentingEntity rentingEntity = modelMapper.map(renting, RentingEntity.class);
         rentingRepository.save(rentingEntity);
         return renting;
     }
 
+    @Transactional(readOnly = false)
     private void changeCarStatusToRented(Vehicle vehicle) {
         vehicle.setRented(true);
         vehicleService.editVehicle(vehicle);
